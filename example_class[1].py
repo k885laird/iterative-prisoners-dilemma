e0a3a47f88fa8a80b88f2adf5a2cd262ca63bea3
k885laird <49960951+k885laird@users.noreply.github.com>
2020-02-24 08:54:37 -0600

    Update example_class[1].py

--- example_class[1].py
+++ example_class[1].py
@@ -1,14 +1,26 @@
-class Student(object):
-    def __init__(self, height=65, weight=130):
-        self.height = height
-        self.weight = weight
-    def grow(self, change=1):
-        self.height += change
+####
+# Each team's file must define four tokens:
+#     team_name: a string
+#     strategy_name: a string
+#     strategy_description: a string
+#     move: A function that returns 'c' or 'b'
+####
 
-'''The following code can be executed in the IPython session:
-In[]:   alex = Student(height=72)   # Creates a Student object called alex with a height of 72.
-In[]:   print alex.weight           # Prints the weight attribute of the alex object.
-Out[]:  130                         # The output will be 130 becuase that is the default weight and was not changed.
-In[]:   alex.grow(2)                # Calls the grow method on the object alex
-In[]:   print alex.height           # Prints the current height of the alex object.
-'''
+team_name = 'Team NN'
+strategy_name = 'Collude'
+strategy_description = 'Always collude.'
+    
+def move(my_history, their_history, my_score, their_score):
+    '''Make my move based on the history with this player.
+    
+    history: a string with one letter (c or b) per round that has been played with this opponent.
+    their_history: a string of the same length as history, possibly empty. 
+    The first round between these two players is my_history[0] and their_history[0]
+    The most recent round is my_history[-1] and their_history[-1]
+    
+    Returns 'c' or 'b' for collude or betray.
+    '''
+    
+    # This player always colludes.
+    return 'c'
+    
